d931ca1d376fa94d6340610033364326a1e56f63
Edward Higgins <106348326+kiereshka@users.noreply.github.com>
2023-12-07 23:08:17 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+import Header from "./components/Header.js";
+import Login from "./components/StateLogin.js";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
